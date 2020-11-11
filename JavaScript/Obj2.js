1df5aef6edffae049a57a17ff457b8fa0e3e4a7e
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2020-11-11 11:24:04 +0530

    iterating obj props

---
+++ JavaScript/Obj2.js
@@ -0,0 +1,14 @@
+var product = {
+  p_id: 101,
+  p_Name: "Iphone12",
+  p_price: "500USD",
+};
+
+for (a in product) {
+  console.log(a);
+  console.log(product.a);
+  console.log(product[a]);
+}
+
+//.a , expecting value
+//[a], assign value
