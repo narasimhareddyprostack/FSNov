cc931d89c0d74bc6672ab631fe5c34bb7b3005c9
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2020-12-24 17:19:17 +0530

    props

--- React25/src/Props/Message.js
+++ React25/src/Props/Message.js
@@ -5,9 +5,11 @@ let Message = (props) => {
     <>
       <h1> Hello, {props.one} </h1>
       <h5> Money : {props.two}</h5>
+      <h1> {props.assets.house}</h1>
       <h1> {props.assets.car}</h1>
+      <h1> {props.assets.cash}</h1>
       <h4> {JSON.stringify(props)}</h4>
-      <p>{props.three()}</p>
+      <p>{props.three()}</p> 
     </>
   );
 };
